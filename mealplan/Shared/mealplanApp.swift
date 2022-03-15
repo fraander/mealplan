0a4b97af57dbfc406a98d307787ff9af1d0a77cb
fraander <feoa.shop@gmail.com>
2022-03-14 18:32:01 -0700

    create project

---
+++ mealplan/Shared/mealplanApp.swift
@@ -0,0 +1,17 @@
+//
+//  mealplanApp.swift
+//  Shared
+//
+//  Created by Frank Anderson on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct mealplanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
